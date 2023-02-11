7002b0dbd23cb7000d56da7605f4f213f6458330
lnalice <rara9915@gmail.com>
2023-02-11 21:10:23 +0900

    Add ch1-p02

---
+++ ch1/P01_FirstNonrepeatedCharacter/Chaser.java
@@ -0,0 +1,24 @@
+package P01_FirstNonrepeatedCharacter;
+
+import java.util.*;
+
+public class Chaser {
+
+	public static char firstNonrepeatedCharacter(String str) {
+		ArrayList<Character> existedTarget = new ArrayList<>();
+		
+		for (int i=0; i <str.length();  i++) {
+			
+			char target = str.charAt(i);
+			CharSequence cs = String.valueOf(target);
+			String subStr = str.substring(i+1);
+			
+			if(!subStr.contains(cs) && !existedTarget.contains(target)) 
+				return target;
+			
+			existedTarget.add(target);
+		}
+		
+		return ' ';
+	}
+}
---
+++ ch1/P01_FirstNonrepeatedCharacter/Main.java
@@ -0,0 +1,38 @@
+package P01_FirstNonrepeatedCharacter;
+
+import java.util.*;
+import java.time.*;
+
+public class Main {
+	
+	private static double[] nowToSeconds() {
+		double nanoseconds = LocalTime.now().toNanoOfDay();
+		double milliseconds = nanoseconds*(Math.pow(10, -6));
+		double[] result = {nanoseconds, milliseconds}; 
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		String str = in.nextLine();
+		
+		in.close();
+		
+		double[] startTime = nowToSeconds();
+		
+		char chasedCharacter = Chaser.firstNonrepeatedCharacter(str);
+		
+		double[] endTime = nowToSeconds();
+		
+		double executionNanoseconds = endTime[0] - startTime[0];
+		double executionMilliseconds = endTime[1] - startTime[1];
+		
+		
+		System.out.printf("Execution time : %d ns ( %d ms )\n", (long) executionNanoseconds, (long) executionMilliseconds);
+		
+		System.out.println("Found Character : " + chasedCharacter);
+				
+	}
+
+}
