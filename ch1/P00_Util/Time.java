32b9786add686d9701d5e2b90707c371cca4a4a1
lnalice <rara9915@gmail.com>
2023-03-05 16:31:40 +0900

    Add time_function_ver3

--- ch1/P00_Util/Time.java
+++ ch1/P00_Util/Time.java
@@ -3,6 +3,16 @@ package P00_Util;
 import java.time.*;
 
 public class Time {
+	
+	private static double startNanoseconds;
+	private static double endNanoseconds;
+	private static double startMilliseconds;
+	private static double endMilliseconds;
+	
+	/**
+	 * time function ver1
+	 * (for p01)
+	 */
 	public static double[] nowtoSecondandMillisecond() {
 		double nanoseconds = LocalTime.now().toNanoOfDay();
 		double milliseconds = nanoseconds*(Math.pow(10, -6));
@@ -10,8 +20,9 @@ public class Time {
 		return result;
 	}
 	
+	
 	/**
-	 * 
+	 * time function ver2
 	 * @param startTime double [nanoseconds, milliseconds]
 	 * @param endTime double [nanoseconds, milliseconds]
 	 */
@@ -33,12 +44,34 @@ public class Time {
 		double milliseconds = nowToNanoSeconds()*(Math.pow(10, -6));
 		return milliseconds;
 	}
-	
+
 	public static long executionTime (double startTime, double endTime) {
 		return (long) (endTime - startTime);
 	}
 	
 	public static void displayExecutionNanosecondsAndMilliseconds (long executionNanoseconds, long executionMilliseconds) {
 		System.out.printf("Execution time : %d ns ( %d ms )\n", (long) executionNanoseconds, (long) executionMilliseconds);
+	}
+	
+	
+	/**
+	 * time function ver3
+	 * (use private static variable)
+	 */
+	public static void setStartTime () {
+		startNanoseconds = (double) LocalTime.now().getNano();
+		startMilliseconds = startNanoseconds * (Math.pow(10,  -6));
+	}
+	
+	public static void setEndTime () {
+		endNanoseconds = (double) LocalTime.now().getNano();
+		endMilliseconds = endNanoseconds * (Math.pow(10,  -6));
+	}
+	
+	public static void displayExecutionTime () {
+		long executionNanoseconds = (long) (endNanoseconds - startNanoseconds);
+		long executionMilliseconds = (long) (endMilliseconds - startMilliseconds);
+		
+		System.out.printf("Execution time : %d ns ( %d ms )\n", executionNanoseconds, executionMilliseconds);
 	}
 }
--- ch1/P02_FirstNonrepeatedCharacter/Main.java
+++ ch1/P02_FirstNonrepeatedCharacter/Main.java
@@ -2,16 +2,25 @@ package P02_FirstNonrepeatedCharacter;
 
 import java.util.*;
 import P00_Util.Time;
+import P06_CountOccurrencesOfCharacter.Words;
 
 public class Main {
 	
+//	final static String INPUT_TEXT = "Let's find a First Nonrepeated Character.";
+	
 	public static void main(String[] args) {
+		
+		System.out.println("P02 - First Nonrepeated Character");
+		System.out.println("Pleas write input text : ");
+		
 		Scanner in = new Scanner(System.in);
 		
 		String str = in.nextLine();
 		
 		in.close();
 				
+		System.out.println("\nArrayList soultion:");
+		
 		double[] startTime = Time.nowtoSecondandMillisecond();
 		
 		char chasedCharacter = Chaser.firstNonrepeatedCharacter(str);
@@ -25,6 +34,21 @@ public class Main {
 		System.out.printf("Execution time : %d ns ( %d ms )\n", (long) executionNanoseconds, (long) executionMilliseconds);
 		
 		System.out.println("Found Character : " + chasedCharacter);
+		
+
+		System.out.println("\nArrayList soultion:");
+		
+		Time.setStartTime();
+						
+		char chasedCharacter2 = Chaser.firstNonrepeatedCharacter(str);
+		
+		Time.setEndTime();
+		
+		Time.displayExecutionTime();
+		
+		
+		System.out.println("Found Character : " + chasedCharacter2);
+		
 				
 	}
 
